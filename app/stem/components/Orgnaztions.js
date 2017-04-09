0a033f16c8cf08122b3fb4704062e71dd1e36cd7
Jawnkuin <jawnkuin@gmail.com>
2017-04-10 00:24:02 +0800

    fix tree

--- app/stem/components/Orgnaztions.js
+++ app/stem/components/Orgnaztions.js
@@ -7,52 +7,65 @@ const TreeNode = Tree.TreeNode;
 const data = {
   orgs: [
     {
+      id: '0',
       name: '集团领导',
       total: 5,
       online: 1,
       members: [
         {
-          name: '陈武'
+          id: '0-1',
+          name: '陈武0'
         },
         {
-          name: '吴建军'
+          id: '0-2',
+          name: '陈武1'
         },
         {
-          name: '吴建军'
+          id: '0-3',
+          name: '陈武2'
         }
       ]
     },
     {
+      id: '1',
       name: '经纬测绘信息有限公司',
       total: 27,
       online: 2,
       members: [
         {
+          id: '1-1',
           name: '吴建军'
         },
         {
+          id: '1-2',
           name: '饶强'
         },
         {
+          id: '1-3',
           name: '高宇婷'
         },
         {
+          id: '1-4',
           name: '鲁东'
         }
       ]
     },
     {
+      id: '2',
       name: '六一八信息技术有限公司',
       total: 5,
       online: 0,
       members: [
         {
+          id: '2-1',
           name: '王文伟'
         },
         {
+          id: '2-2',
           name: '林巍'
         },
         {
+          id: '2-1',
           name: '吴良堤'
         }
 
@@ -61,10 +74,30 @@ const data = {
   ]
 };
 
-const getTree = () => {
-  data.orgs.map((org) => {
-
-  });
+const getTreeNodes = (node) => {
+  if (node.orgs) {
+    return (
+      <TreeNode
+        title={`${node.name} ${node.online}/${node.total}`}
+        key={node.id}
+      >
+        {node.orgs.map(getTreeNodes)}
+      </TreeNode>
+    );
+  }
+  if (node.members) {
+    return (
+      <TreeNode
+        title={`${node.name} ${node.online}/${node.total}`}
+        key={node.id}
+      >
+        {node.members.map(getTreeNodes)}
+      </TreeNode>
+    );
+  }
+  return (
+    <TreeNode title={node.name} key={node.id} />
+  );
 };
 
 // 主面板列表项
@@ -72,14 +105,6 @@ export default () => (
   <Tree
     className={styles.BuddyItem}
   >
-    <TreeNode title={'集团领导'} key="0-0">
-      <TreeNode title="parent 1-0" key="0-0-0">
-        <TreeNode title="leaf" key="0-0-0-0" />
-        <TreeNode title="leaf" key="0-0-0-1" />
-      </TreeNode>
-      <TreeNode title="parent 1-1" key="0-0-1">
-        <TreeNode title={<span style={{ color: '#08c' }}>sss</span>} key="0-0-1-0" />
-      </TreeNode>
-    </TreeNode>
+    {data.orgs.map(org => getTreeNodes(org))}
   </Tree>
 );
